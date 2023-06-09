b6d865425fea0b610d549b79fc95789d3c491c9b
Humphrey Drummond <humphrey.drummond@atombank.co.uk>
2023-06-09 12:59:14 +0100

    0.2.0 changing aprint to print and adding

---
+++ lofigui/context.py
@@ -0,0 +1,32 @@
+import asyncio
+
+
+class PrintContext:
+    def __init__(self):
+        self.queue = asyncio.Queue()
+        self.buffer = ""  # This is a results buffer
+
+    def read(self):
+        if self.queue.empty():
+            return
+        response = ""
+        while not self.queue.empty():
+            # Get a "work item" out of the queue.
+            response += self.queue.get_nowait()
+            self.queue.task_done()
+        self.buffer += response
+
+
+# Slightly more involved but allows both single threaded use and option multithreaded
+_ctx = PrintContext()
+
+def buffer(ctx=None):
+    if ctx is None:
+        ctx = _ctx
+    ctx.read()  # drain buffer
+    return ctx.buffer
+
+def reset(ctx=None):
+    if ctx is None:
+        ctx = _ctx
+    ctx.buffer = ""
---
+++ lofigui/options.py
@@ -0,0 +1,25 @@
+
+
+# <div class = "checkbox">
+#                <label class = "is-size-5">Fruits</label>
+#                <br>
+#                <label class = "checkbox">
+#                   <input type = "checkbox">
+#                   Orange
+#                </label>
+               
+#                <label class = "checkbox">
+#                   <input type = "checkbox">
+#                   Apple
+#                </label>
+               
+#                <label class = "checkbox">
+#                   <input type = "checkbox">
+#                   Grapes
+#                </label>
+               
+#                <label class = "checkbox">
+#                   <input type = "checkbox">
+#                   Mango
+#                </label>
+#             </div>
